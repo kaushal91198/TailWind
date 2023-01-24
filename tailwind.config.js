742332c4220a2ac898eeb6e00fd43ab51a716e2c
Kaushal Panchal <92462707+kaushal91198@users.noreply.github.com>
2023-01-24 11:59:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: "100px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      spacing: { 13: "3.25rem" },
+      fontSize: { "10xl": ["9rem", { lineHeight: "2" }] },
+     
+    },
+  },
+  plugins: [],
+};
